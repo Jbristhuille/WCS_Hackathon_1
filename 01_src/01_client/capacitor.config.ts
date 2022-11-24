62f1891adc9a8abc33a63ce7e27bfb8c05190824
Jbristhuille <jbristhuille@gmail.com>
2022-11-24 09:31:52 +0100

    :tada: Start project
    * Setup client folder
    * Setup server folder
    * Create dev scripts

---
+++ 01_src/01_client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wcs_hackathon_travel',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
