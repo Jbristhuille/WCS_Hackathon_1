9374fadb869b4065510eb1158a7c6655e665df75
Jbristhuille <jbristhuille@gmail.com>
2022-11-24 10:22:40 +0100

    :fire: Clean server code
    * Remove app.service
    * Add header on app controller file

--- 01_src/02_server/src/app.controller.ts
+++ 01_src/02_server/src/app.controller.ts
@@ -1,12 +1,12 @@
+/**
+ * @ Author: Jbristhuille
+ * @ Create Time: 2022-11-24 09:19:38
+ * @ Description: Main module controller
+ */
+
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor() {}
 }
