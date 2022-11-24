a80414139464f1a501d9620e95ad4f248135b27b
Jbristhuille <jbristhuille@gmail.com>
2022-11-24 10:48:07 +0100

    :fire: Clean client code
    * Remove Page.tsx
    * Create Home.tsx (replace Page.tsx)
    * Rework routing
    * Rework menu
    * Remove ExploreContainer.tsx

--- 01_src/01_client/src/App.tsx
+++ 01_src/01_client/src/App.tsx
@@ -1,27 +1,47 @@
+/**
+ * @ Author: Jbristhuille
+ * @ Create Time: 2022-11-24 09:07:46
+ * @ Description: Main project component
+ */
+
+/* SUMMARY
+  * Ionic
+  * React
+  * Pages
+  * Components
+  * Styles
+*/
+
+/* Ionic */
 import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
+/***/
+
+/* React */
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
+/***/
+
+/* Pages */
+import Home from './pages/Home';
+/***/
+
+/* Components */
 import Menu from './components/Menu';
-import Page from './pages/Page';
+/***/
 
-/* Core CSS required for Ionic components to work properly */
+/* Styles */
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-
-/* Theme variables */
 import './theme/variables.css';
+/***/
 
 setupIonicReact();
 
@@ -33,10 +53,10 @@ const App: React.FC = () => {
           <Menu />
           <IonRouterOutlet id="main">
             <Route path="/" exact={true}>
-              <Redirect to="/page/Inbox" />
+              <Redirect to="/home" />
             </Route>
-            <Route path="/page/:name" exact={true}>
-              <Page />
+            <Route path="/home" exact={true}>
+              <Home />
             </Route>
           </IonRouterOutlet>
         </IonSplitPane>
